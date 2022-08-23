adf84223773e6222853b44e0da0ef14f2fdafedc
DevilShine <daniilburkovskyi@gmail.com>
2022-08-23 17:00:12 +0300

    Draw

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ wrap.append(wrapField);
 
 var count = 1;
 
+var numX = 0;
+var numO = 0;
+
 var wrapFieldClick = document.querySelector(".wrap-field");
 wrapFieldClick.addEventListener("click", function (e) {
   var elem = e.target;
@@ -25,8 +28,10 @@ wrapFieldClick.addEventListener("click", function (e) {
     if (elem.innerHTML == "") {
       if (count % 2 == 1) {
         elem.innerHTML = "X";
+        numX++;
       } else if (count % 2 == 0) {
         elem.innerHTML = "O";
+        numO++;
       }
     } else {
       count--;
@@ -38,13 +43,20 @@ wrapFieldClick.addEventListener("click", function (e) {
       document.querySelector(".btn-" + i).innerHTML = "";
     }
     alert("WIner X");
-  }
-
-  if (checkWiner(0) == "O" && checkWiner(1) == "O" && checkWiner(2) == "O" || checkWiner(3) == "O" && checkWiner(4) == "O" && checkWiner(5) == "O" || checkWiner(6) == "O" && checkWiner(7) == "O" && checkWiner(8) == "O" || checkWiner(0) == "O" && checkWiner(3) == "O" && checkWiner(6) == "O" || checkWiner(1) == "O" && checkWiner(4) == "O" && checkWiner(7) == "O" || checkWiner(2) == "O" && checkWiner(5) == "O" && checkWiner(8) == "O" || checkWiner(0) == "O" && checkWiner(4) == "O" && checkWiner(8) == "O" || checkWiner(2) == "O" && checkWiner(4) == "O" && checkWiner(6) == "O") {
+  } else if (checkWiner(0) == "O" && checkWiner(1) == "O" && checkWiner(2) == "O" || checkWiner(3) == "O" && checkWiner(4) == "O" && checkWiner(5) == "O" || checkWiner(6) == "O" && checkWiner(7) == "O" && checkWiner(8) == "O" || checkWiner(0) == "O" && checkWiner(3) == "O" && checkWiner(6) == "O" || checkWiner(1) == "O" && checkWiner(4) == "O" && checkWiner(7) == "O" || checkWiner(2) == "O" && checkWiner(5) == "O" && checkWiner(8) == "O" || checkWiner(0) == "O" && checkWiner(4) == "O" && checkWiner(8) == "O" || checkWiner(2) == "O" && checkWiner(4) == "O" && checkWiner(6) == "O") {
     for (var i = 0; i < 9; i++) {
       document.querySelector(".btn-" + i).innerHTML = "";
     }
     alert("WIner O");
+  }
+
+  if (numX == 5 || numO == 5) {
+    for (var i = 0; i < 9; i++) {
+      document.querySelector(".btn-" + i).innerHTML = "";
+    }
+    numX = 0;
+    numO = 0;
+    alert("Draw")
   }
 
   count++;
